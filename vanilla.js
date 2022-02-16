9669b5419d868ba980e330501fcc3228a26dc285
minoo <minoovahedi23@gmail.com>
2022-02-16 18:23:41 +0330

    added js for weather conditions

--- vanilla.js
+++ vanilla.js
@@ -1,1 +1,17 @@
-
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "3fbdb2cb00e76a2efa935fc6f7334120";
+let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apikey}&units=metric`;
+axios.get(apiurl).then(displayTemperature);
