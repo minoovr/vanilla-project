55994454b4d92acc90ed7c512d6af9c778d4d9b8
minoo <minoovahedi23@gmail.com>
2022-02-18 00:06:47 +0330

    fixing the icon

--- vanilla.js
+++ vanilla.js
@@ -27,13 +27,20 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#nowtime");
+  let iconElement = document.querySelector("#image-icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 let apiKey = "3fbdb2cb00e76a2efa935fc6f7334120";
 let city = "Paris";
 let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
